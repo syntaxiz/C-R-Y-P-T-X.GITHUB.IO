21ac428e76a0ec4596f999d3091b072a06671826
C-R-Y-P-T-X <brewandbaker@gmail.com>
2016-05-11 01:39:34 +0400

    Update multi_widget.js

--- multi_widget.js
+++ multi_widget.js
@@ -101,7 +101,7 @@ for (var i = 0; i < crypt_multi_num_cur; ++i) {
 }
 
 if (crypt_multi_display_time)
-    document.write('<div style="font-size:70%;opacity:0.6;padding-top:10px;padding-left:10px;">Updated ' + day + ' ' + month + ' ' + year + ' '
+    document.write('<div style="font-size:40%;opacity:0.6;padding-top:10px;padding-left:10px;">Updated ' + day + ' ' + month + ' ' + year + ' '
         + hour + ':' + minutes + ':' + seconds + ' '
         + time_zone + '</div>');
 

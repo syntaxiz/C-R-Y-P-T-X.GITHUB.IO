f0eae31d42d81bae7f80129e817e1e4d572fdb6c
C-R-Y-P-T-X <brewandbaker@gmail.com>
2016-05-10 22:26:37 +0400

    Update multi_widget.js

--- javascripts/multi_widget.js
+++ javascripts/multi_widget.js
@@ -106,4 +106,3 @@ if (crypt_multi_display_time)
         + time_zone + '</div>');
 
 document.write('<div style="font-size:10px;opacity:0.6;padding:10px;">Powered by <a style="color:' + crypt_multi_font_color + ';" href="https://www.cryptonator.com" target="_blank">Cryptonator</a></div>\
-</div>');

e515a206e0f78c325ce03ab0a450dbd830f5b772
C-R-Y-P-T-X <brewandbaker@gmail.com>
2016-06-05 11:28:14 +0300

    Update calc_widget.js

--- calc_widget.js
+++ calc_widget.js
@@ -11,7 +11,7 @@ function httpGet(theUrl)
 crypt_calc_background_color = typeof crypt_calc_background_color == "undefined" ? "#FFFFFF" : crypt_calc_background_color;
 crypt_calc_transperency = typeof crypt_calc_transperency == "undefined" ? true : crypt_calc_transperency;
 crypt_calc_border_width = typeof crypt_calc_border_width == "undefined" ? 1 : crypt_calc_border_width;
-crypt_calc_border_color = typeof crypt_calc_border_color == "undefined" ? "#CCCCCC" : crypt_calc_border_color;
+crypt_calc_border_color = typeof crypt_calc_border_color == "undefined" ? "#2ECC40" : crypt_calc_border_color;
 crypt_calc_border_corners = typeof crypt_calc_border_corners == "undefined" ? "rounded" : crypt_calc_border_corners;
 crypt_calc_font_family = typeof crypt_calc_font_family == "undefined" ? "Arial" : crypt_calc_font_family;
 crypt_calc_font_size = typeof crypt_calc_font_size == "undefined" ? "medium" : crypt_calc_font_size;
